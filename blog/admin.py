6a90e4aef050038b0f1741281e0f8e7f6f67431f
Mohamed El-Sharqawy <dev.elbehery@gmail.com>
2024-07-26 05:25:03 +0300

    store data into db, fetch it and display it dynamically in views

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Post, Author, Tag
 
 # Register your models here.
+
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
+    list_display = ("title", "author", "date")
+    list_filter = ("author", "tags", "date")
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "email_address")
+    list_filter = ("first_name", "last_name", "email_address")
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("caption",)
+    list_filter = ("caption",)
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Tag, TagAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
+
 
 # Create your models here.
+class Tag(models.Model):
+    caption = models.CharField(max_length=50)
+
+    def __str__(this):
+        return f"{this.caption}"
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email_address = models.EmailField()
+
+    def __str__(this):
+        return f"{this.first_name} {this.last_name}"
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    excerpt = models.CharField(max_length=200)
+    image_name = models.CharField(max_length=100)
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(unique=True, db_index=True, max_length=200)
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    author = models.ForeignKey(
+        to=Author, on_delete=models.SET_NULL, null=True, related_name="posts"
+    )
+    tags = models.ManyToManyField(to=Tag, related_name="posts")
+    
+    def __str__(this):
+        return f"{this.tags} Was Written By {this.author}"
